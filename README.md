8eab5d6a21a236e282f9648e5b05ff6cb3e6db55
Sanya124 <tolstihin1996@mail.ru>
2022-07-24 03:41:33 +0700

    first API-test

---
+++ README.md
@@ -0,0 +1,9 @@
+# Learning to automate testing in Python
+Implemented the first test on pytest. <br>
+The tested service is presented here -> https://github.com/touchbit/automatron
+
+###config.py
+contains url (Need to change when starting locally) for of service under test:
+```commandline
+HOST = 'http://localhost:8080'
+```
---
+++ bugs.py
@@ -0,0 +1,28 @@
+""" Class for testing service for obtaining information on defects present in the system. """
+from requests import Response, get
+
+from config import HOST
+
+
+class Bugs:
+    def get_bugs(self, bug_id, authentication_data, path='/api/bugs/{path}') -> Response:
+        """ Get a description of bug by id.
+
+        Args:
+            bug_id -                id of bugs. 1 of 13
+            authentication_data -   authentication data (usage access_token and refresh_token) for headers of requests
+            path -                  request url
+
+        Returns:
+            response -              object of class Response
+        """
+        headers = {
+            'access_token': 'Bearer ' + authentication_data['access_token'],
+            'refresh_token': authentication_data['refresh_token']
+        }
+        response = get(
+            url=HOST + path.format(path=bug_id),
+            headers=headers
+        )
+
+        return response
